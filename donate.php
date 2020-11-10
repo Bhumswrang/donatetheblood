5f4003f6511e4a4d6707a8f01f86e9c8bd13ee08
Bhum <bhumn98@gmail.com>
2020-11-11 00:13:34 +0530

    donate.php connected to database donatethe blood(donor table)

--- donate.php
+++ donate.php
@@ -81,7 +81,7 @@
 
 		//   validation program for blood group
 		if(isset($_POST['blood_group']) && !empty($_POST['blood_group'])){
-			$year= $_POST['blood_group'];
+			$blood_group= $_POST['blood_group'];
 		  }
 		  else{
 			$blood_groupError = '<div class="alert alert-danger alert-dismissible fade show" role="alert">
@@ -119,7 +119,7 @@
 		//  validation program for contact number
 		if(isset($_POST['contact_no']) && !empty($_POST['contact_no'])){
 			//check city with the pattern of contact_no
-			if( preg_match('/\d{11}/', $_POST['contact_no']) ){
+			if( preg_match('/\d{10}/', $_POST['contact_no']) ){
 				$contact=$_POST['contact_no'];
 			}
 			else{
@@ -175,7 +175,7 @@
 			//validation program for email
 			if(isset($_POST['email']) && !empty($_POST['email'])){
 				//check city with the pattern of email
-				$pattern='/^ [a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$/';
+				$pattern= "/^([a-z0-9\+_\-]+)(\.[a-z0-9\+_\-]+)*@([a-z0-9\-]+\.)+[a-z]{2,6}$/ix";
 				if( preg_match($pattern, $_POST['email']) ){
 					$check_email=$_POST['email'];
 					$sql= "SELECT email from donor WHERE email=$check_email";
@@ -208,6 +208,30 @@
 				<span aria-hidden="true">&times;</span>
 				</button>
 			</div>';
+			}
+
+			// insertion query to database 
+			if(isset($name) && isset($gender) && isset($day) && isset($month) && isset($year) && isset($city) && isset($contact) && isset($password) && isset($email)){
+				$donorDOB = $day."-".$month."-".$year;
+				$sql= "INSERT INTO donor (`name`, `gender`, `dob`, `city`, `contact_no`, `save_life_date`, `password`, `email`) VALUES ('$name', '$gender', '$donorDOB', '$city', '$contact', '0', '$password', '$email')";
+
+				if(mysqli_query($connection,$sql)){
+					$submitSuccess = '<div class="alert alert-success alert-dismissible fade show" role="alert">
+					<strong>Data inserted Successfully.</strong>
+					<button type="button" class="close" data-dismiss="alert" aria-label="Close">
+					  <span aria-hidden="true">&times;</span>
+					</button>
+				  </div>';
+				}
+				else{
+					$sumbitError = '<div class="alert alert-danger alert-dismissible fade show" role="alert">
+					<strong>Data is not inserted,Try again.</strong>
+					<button type="button" class="close" data-dismiss="alert" aria-label="Close">
+					  <span aria-hidden="true">&times;</span>
+					</button>
+				  </div>';
+
+				}
 			}
 		
 // End of if condition in term validation
@@ -253,6 +277,10 @@ box-shadow: 0px 2px 5px -2px rgba(89,89,89,0.95);
 	.red-bar{
 		width: 35%;
 	}
+	.bhum{
+		padding: .375rem .25rem;
+		margin: 0.2em;
+	}
 </style>
 <div class="container-fluid red-background size">
 	<div class="row">
@@ -269,7 +297,11 @@ box-shadow: 0px 2px 5px -2px rgba(89,89,89,0.95);
 					<hr class="red-bar">
 						
           <!-- Error Messages -->
-		  <?php	if(isset($termError)) echo $termError; ?>
+		  <?php	
+		  if(isset($termError)) echo $termError; 
+		  if(isset($submitSuccess)) echo $submitSuccess;
+		  if(isset($sumbitError)) echo $sumbitError;
+		  ?>
 
 				<form class="form-group" action="" method="post" novalidate="">
 					<div class="form-group">
@@ -303,16 +335,16 @@ box-shadow: 0px 2px 5px -2px rgba(89,89,89,0.95);
 					<?php	if(isset($genderError)) echo $genderError; ?>
 				    </div><!--gender-->
 				    <div class="form-inline">
-              <label for="name">Date of Birth</label><br>
-              <select class="form-control demo-default" id="date" name="day" style="margin-bottom:10px;" required>
+              <label for="name">Date of Birth &nbsp</label><br>
+              <select class="form-control demo-default bhum" id="date" name="day" style="margin-bottom:10px;" required>
                 <option value="">---Date---</option>
                 <option value="01" >01</option><option value="02" >02</option><option value="03" >03</option><option value="04" >04</option><option value="05" >05</option><option value="06" >06</option><option value="07" >07</option> <option value="08" >08</option><option value="09" >09</option><option value="10" >10</option><option value="11" >11</option><option value="12" >12</option><option value="13" >13</option><option value="14" >14</option><option value="15" >15</option><option value="16" >16</option><option value="17" >17</option><option value="18" >18</option><option value="19" >19</option><option value="20" >20</option><option value="21" >21</option><option value="22" >22</option><option value="23" >23</option><option value="24" >24</option><option value="25" >25</option><option value="26" >26</option><option value="27" >27</option><option value="28" >28</option><option value="29" >29</option><option value="30" >30</option><option value="31" >31</option>
               </select>
-              <select class="form-control demo-default" name="month" id="month" style="margin-bottom:10px;" required>
+              <select class="form-control demo-default bhum" name="month" id="month" style="margin-bottom:10px;" required>
                 <option value="">---Month---</option>
                 <option value="01" >January</option><option value="02" >February</option><option value="03" >March</option><option value="04" >April</option><option value="05" >May</option><option value="06" >June</option><option value="07" >July</option><option value="08" >August</option><option value="09" >September</option><option value="10" >October</option><option value="11" >November</option><option value="12" >December</option>
               </select>
-              <select class="form-control demo-default" id="year" name="year" style="margin-bottom:10px;" required>
+              <select class="form-control demo-default bhum" id="year" name="year" style="margin-bottom:10px;" required>
                 <option value="">---Year---</option>
                 <option value="1957" >1957</option><option value="1958" >1958</option><option value="1959" >1959</option><option value="1960" >1960</option><option value="1961" >1961</option><option value="1962" >1962</option><option value="1963" >1963</option><option value="1964" >1964</option><option value="1965" >1965</option><option value="1966" >1966</option><option value="1967" >1967</option><option value="1968" >1968</option><option value="1969" >1969</option><option value="1970" >1970</option><option value="1971" >1971</option><option value="1972" >1972</option><option value="1973" >1973</option><option value="1974" >1974</option><option value="1975" >1975</option><option value="1976" >1976</option><option value="1977" >1977</option><option value="1978" >1978</option><option value="1979" >1979</option><option value="1980" >1980</option><option value="1981" >1981</option><option value="1982" >1982</option><option value="1983" >1983</option><option value="1984" >1984</option><option value="1985" >1985</option><option value="1986" >1986</option><option value="1987" >1987</option><option value="1988" >1988</option><option value="1989" >1989</option><option value="1990" >1990</option><option value="1991" >1991</option><option value="1992" >1992</option><option value="1993" >1993</option><option value="1994" >1994</option><option value="1995" >1995</option><option value="1996" >1996</option><option value="1997" >1997</option><option value="1998" >1998</option><option value="1999" >1999</option>
               </select>
